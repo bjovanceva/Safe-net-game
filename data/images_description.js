fa356e49014d00521d65e429dfabfa60b6a62518
bjovanceva <bjovanceva3@gmail.com>
2026-01-23 21:10:45 +0100

    added images_description js file

---
+++ data/images_description.js
@@ -0,0 +1,21 @@
+
+const images_description={
+    good1_final: "A person is logging into an account on a smartphone while a two-factor authentication code is sent to a second device",
+    good2_final: "A laptop displays a digital password manager interface designed to store and organize multiple encrypted login credentials",
+    good3_final: "A login page requests an activation code and login credentials for the user Aleksandar",
+    good4_final: "A Windows update installation progress screen, indicating the process is 45% complete",
+    good5_final: "A 'Secure VPN' application showing a connected and encrypted status with a disconnect option",
+    good6_final: "Reporting an email from 'support@paypal-verify.com' with an urgent message to claim a free gift and a link",
+    good7_final: "A system permission prompt from the Instagram app requesting access to user's device's contacts and location, with 'Deny' selected",
+
+    bad1_final: "A social media interface prompting the user to share a post publicly",
+    bad2_final: "An email claiming an account has been suspended and urging the recipient to click a 'VERIFY NOW' link",
+    bad3_final: "Peter asks the recipient if a picture at a provided link is of them",
+    bad4_final: "A web form inviting the user to take a personalized quiz through a shortened link",
+    bad5_final: "A user is connecting to an open, unsecured network named 'CoffeeShop_Free_WiFi' with a warning that it may be unsafe",
+    bad6_final: "Visiting a link through a notification that warns a suspicious link has been detected",
+    bad7_final: "Allowing 'FotoFunApp' to access the device’s contact list and location data",
+    bad8_final: "An email from 'Your Mailchimp Account' that warns of a suspicious login and urges the user to reactivate the account by clicking a link",
+}
+
+export default images_description;
